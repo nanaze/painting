622e76da9b5b921057695565ff8976b1ec1eeabc
Nathan Naze <nanaze@gmail.com>
2020-12-22 21:44:25 -0800

    add script for jay

---
+++ jay_grid.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# Draws a grid on the image. Expects image in std input, writes image to stdout.
+#
+# Original image at https://www.santacruzmuseum.org/wp-content/uploads/2020/03/California_Scrub-Jay_26994352448-1024x683.jpg
+
+from PIL import Image, ImageDraw
+
+import sys
+import urllib.request
+
+def _DrawGrid(img):
+  draw = ImageDraw.Draw(img)
+
+  width, height = img.size
+
+  horizontal_line_count = 10
+  vertical_line_count = 10
+  
+  for i in range(0, vertical_line_count + 1):
+    x = int((i / vertical_line_count) * width)
+    draw.line((x, 0, x, height), fill=128)
+
+  for i in range(0, horizontal_line_count + 1):
+    y = int((i / horizontal_line_count) * height)
+    draw.line((0, y, width, y), fill=128)    
+
+def main():
+  with Image.open(sys.stdin.buffer) as img:
+    _DrawGrid(img)
+    img.save(sys.stdout.buffer, "PNG")
+      
+if __name__ == '__main__':
+  main()
